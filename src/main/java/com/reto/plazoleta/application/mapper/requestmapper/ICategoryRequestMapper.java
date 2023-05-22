23590d74880ba3592032de0112ba949e4c2b491f
JohanaForero <johanaforeromendoza31@gmail.com>
2023-05-22 22:44:26 +0200

    archivos que no pertenecen a esta PR

---
+++ src/main/java/com/reto/plazoleta/application/mapper/requestmapper/ICategoryRequestMapper.java
@@ -0,0 +1,4 @@
+package com.reto.plazoleta.application.mapper.requestmapper;
+
+public interface ICategoryRequestMapper {
+}
---
+++ src/main/java/com/reto/plazoleta/application/mapper/responsemapper/ICategoryResponseMapper.java
@@ -0,0 +1,4 @@
+package com.reto.plazoleta.application.mapper.responsemapper;
+
+public interface ICategoryResponseMapper {
+}
