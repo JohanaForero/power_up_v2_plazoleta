17f32339941a55fc33307ef25cb6c238c0fe2bd7
JohanaForero <johanaforeromendoza31@gmail.com>
2023-05-22 20:02:33 +0200

    archivos que no pertenecen a esta PR

--- src/main/java/com/reto/plazoleta/domain/model/EmployeeRestaurantModel.java
+++ src/main/java/com/reto/plazoleta/domain/model/EmployeeRestaurantModel.java
@@ -3,27 +3,21 @@ package com.reto.plazoleta.domain.model;
 public class EmployeeRestaurantModel {
     private Long idEmployeeRestaurant;
     private RestaurantModel idRestaurant;
-
     public EmployeeRestaurantModel() {
     }
-
     public EmployeeRestaurantModel(Long idEmployeeRestaurant, RestaurantModel idRestaurant) {
         this.idEmployeeRestaurant = idEmployeeRestaurant;
         this.idRestaurant = idRestaurant;
     }
-
     public Long getIdEmployeeRestaurant() {
         return idEmployeeRestaurant;
     }
-
     public void setIdEmployeeRestaurant(Long idEmployeeRestaurant) {
         this.idEmployeeRestaurant = idEmployeeRestaurant;
     }
-
     public RestaurantModel getIdRestaurant() {
         return idRestaurant;
     }
-
     public void setIdRestaurant(RestaurantModel idRestaurant) {
         this.idRestaurant = idRestaurant;
     }
--- src/main/java/com/reto/plazoleta/domain/model/RestaurantModel.java
+++ src/main/java/com/reto/plazoleta/domain/model/RestaurantModel.java
@@ -1,7 +1,6 @@
 package com.reto.plazoleta.domain.model;
 
 public class RestaurantModel {
-
     private Long idRestaurant;
     private String name;
     private String address;
@@ -12,58 +11,54 @@ public class RestaurantModel {
 
     public RestaurantModel() {}
 
+    public RestaurantModel(String name, String address, String phone, String urlLogo, Long nit, Long idOwner) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.urlLogo = urlLogo;
+        this.nit = nit;
+        this.idOwner = idOwner;
+    }
+
     public Long getIdRestaurant() {
         return idRestaurant;
     }
-
     public void setIdRestaurant(Long idRestaurant) {
         this.idRestaurant = idRestaurant;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getAddress() {
         return address;
     }
-
     public void setAddress(String address) {
         this.address = address;
     }
-
     public String getPhone() {
         return phone;
     }
-
     public void setPhone(String phone) {
         this.phone = phone;
     }
-
     public String getUrlLogo() {
         return urlLogo;
     }
-
     public void setUrlLogo(String urlLogo) {
         this.urlLogo = urlLogo;
     }
-
     public Long getNit() {
         return nit;
     }
-
     public void setNit(Long nit) {
         this.nit = nit;
     }
-
     public Long getIdOwner() {
         return idOwner;
     }
-
     public void setIdOwner(Long idOwner) {
         this.idOwner = idOwner;
     }
