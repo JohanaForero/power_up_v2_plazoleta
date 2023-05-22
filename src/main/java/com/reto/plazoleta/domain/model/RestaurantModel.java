25b19705089ffab21a70b9c92e6af065ddd27d95
JohanaForero <johanaforeromendoza31@gmail.com>
2023-05-22 23:49:19 +0200

    espacios que no deben ir en RestaurantModel

--- src/main/java/com/reto/plazoleta/domain/model/RestaurantModel.java
+++ src/main/java/com/reto/plazoleta/domain/model/RestaurantModel.java
@@ -1,7 +1,6 @@
 package com.reto.plazoleta.domain.model;
 
 public class RestaurantModel {
-
     private Long idRestaurant;
     private String name;
     private String address;
@@ -21,62 +20,46 @@ public class RestaurantModel {
         this.nit = nit;
         this.idOwner = idOwner;
     }
-
     public Long getIdRestaurant() {
         return idRestaurant;
     }
-
     public void setIdRestaurant(Long idRestaurant) {
         this.idRestaurant = idRestaurant;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getAddress() {
         return address;
     }
-
     public void setAddress(String address) {
         this.address = address;
     }
-
     public String getPhone() {
         return phone;
     }
-
     public void setPhone(String phone) {
         this.phone = phone;
     }
-
     public String getUrlLogo() {
         return urlLogo;
     }
-
     public void setUrlLogo(String urlLogo) {
         this.urlLogo = urlLogo;
     }
-
     public Long getNit() {
         return nit;
     }
-
     public void setNit(Long nit) {
         this.nit = nit;
     }
-
     public Long getIdOwner() {
         return idOwner;
     }
-
     public void setIdOwner(Long idOwner) {
         this.idOwner = idOwner;
     }
-
-
 }
