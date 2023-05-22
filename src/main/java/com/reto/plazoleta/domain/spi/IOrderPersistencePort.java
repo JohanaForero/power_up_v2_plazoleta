10313b1d776b6ce7f0a8275bf9796e98bcc5d4c5
JohanaForero <johanaforeromendoza31@gmail.com>
2023-05-22 21:07:08 +0200

    archivos que no pertenecen a esta PR

---
+++ src/main/java/com/reto/plazoleta/domain/spi/IOrderPersistencePort.java
@@ -0,0 +1,12 @@
+package com.reto.plazoleta.domain.spi;
+
+import com.reto.plazoleta.domain.model.OrderModel;
+
+import java.util.List;
+
+public interface IOrderPersistencePort {
+    OrderModel saveOrder(OrderModel orderModel);
+
+    List<OrderModel> getAllOrders();
+
+}
