dcbc93cff94b1f27d83815e273c1ba39b8fd6a16
JohanaForero <johanaforeromendoza31@gmail.com>
2023-05-24 02:34:53 +0200

    aptualizando el plato

--- src/main/java/com/reto/plazoleta/application/dto/response/UpdateDishResponseDto.java
+++ src/main/java/com/reto/plazoleta/application/dto/response/UpdateDishResponseDto.java
@@ -1,22 +1,6 @@
 package com.reto.plazoleta.application.dto.response;
 
 public class UpdateDishResponseDto {
-    private Double price;
-    private String descriptionDish;
 
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public String getDescriptionDish() {
-        return descriptionDish;
-    }
-
-    public void setDescriptionDish(String descriptionDish) {
-        this.descriptionDish = descriptionDish;
-    }
+    private Long idDish;
 }
